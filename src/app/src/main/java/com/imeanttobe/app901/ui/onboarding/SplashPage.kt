cd6ce64dffa2c9e7103d4ab1047d85a88a301e30
Shawn Kang <csm06063@naver.com>
2025-04-16 23:45:35 +0900

    feat: 기초 네비게이션 구조 작업

---
+++ src/app/src/main/java/com/imeanttobe/app901/ui/onboarding/SplashPage.kt
@@ -0,0 +1,16 @@
+package com.imeanttobe.app901.ui.onboarding
+
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun SplashPage() {
+    Scaffold(
+        modifier = Modifier
+    ) { innerPadding ->
+        Surface(modifier = Modifier.padding(innerPadding)) {}
+    }
+}
---
+++ src/app/src/main/java/com/imeanttobe/app901/ui/recipe/RecipeHomePage.kt
@@ -0,0 +1,16 @@
+package com.imeanttobe.app901.ui.recipe
+
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun RecipeHomePage() {
+    Scaffold(
+        modifier = Modifier
+    ) { innerPadding ->
+        Surface(modifier = Modifier.padding(innerPadding)) {}
+    }
+}
