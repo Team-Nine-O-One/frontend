ef1e1e9ea7d59a267b8fce751f6d5de067ba9fc2
Shawn Kang <shineksu813@outlook.com>
2025-05-08 17:24:28 +0900

    feat: Created RetrofitClient

---
+++ src/app/src/main/java/com/imeanttobe/app901/api/RetrofitClient.kt
@@ -0,0 +1,18 @@
+package com.imeanttobe.app901.api
+
+import com.imeanttobe.app901.BuildConfig
+import com.imeanttobe.app901.api.service.CartService
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+object RetrofitClient {
+    private const val BASE_URL = BuildConfig.API_BASE_URL;
+
+    val cartService: CartService by lazy {
+        Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+            .create(CartService::class.java)
+    }
+}
