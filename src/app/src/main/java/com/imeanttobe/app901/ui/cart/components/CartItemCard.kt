ab715455c42062745d78264c8722cf30297635e2
Shawn Kang <shineksu813@outlook.com>
2025-05-08 15:27:32 +0900

    refactor: Applied package hierarchy changes

--- src/app/src/main/java/com/imeanttobe/app901/ui/cart/components/CartItemCard.kt
+++ src/app/src/main/java/com/imeanttobe/app901/ui/cart/components/CartItemCard.kt
@@ -25,7 +25,7 @@ import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import com.imeanttobe.app901.type.Cart
+import com.imeanttobe.app901.type.data.Cart
 import com.imeanttobe.app901.type.Formatter
 
 @Composable
