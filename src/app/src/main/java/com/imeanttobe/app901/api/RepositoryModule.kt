0bcc2090111bceb5c06037c661cb76a793cc5711
Shawn Kang <shineksu813@outlook.com>
2025-05-15 20:00:38 +0900

    chore: API 모방 여부에 따라 저장소 구현체가 달라지게 구현

--- src/app/src/main/java/com/imeanttobe/app901/api/RepositoryModule.kt
+++ src/app/src/main/java/com/imeanttobe/app901/api/RepositoryModule.kt
@@ -1,31 +1,44 @@
 package com.imeanttobe.app901.api
 
+import com.imeanttobe.app901.BuildConfig
 import com.imeanttobe.app901.api.repo.CartRepo
 import com.imeanttobe.app901.api.repo.FakeCartRepoImpl
 import com.imeanttobe.app901.api.repo.FakeMemoRepoImpl
 import com.imeanttobe.app901.api.repo.MemoRepo
 import com.imeanttobe.app901.api.service.CartService
 import com.imeanttobe.app901.api.service.MemoService
-import dagger.Binds
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
-abstract class RepositoryModule {
-    @Binds
-    abstract fun bindFakeMemoRepo(memoRepoImpl: FakeMemoRepoImpl): MemoRepo
+object RepositoryModule {
+    @Provides
+    @Singleton
+    fun provideCartRepo(cartService: CartService): CartRepo =
+        if (BuildConfig.IS_MOCK_ENABLED) {
+            FakeCartRepoImpl(cartService = cartService)
+        } else {
+            FakeCartRepoImpl(cartService = cartService)
+        }
 
-    @Binds
-    abstract fun bindFakeCartRepo(cartRepoImpl: FakeCartRepoImpl): CartRepo
+    @Provides
+    @Singleton
+    fun provideMemoRepo(memoService: MemoService): MemoRepo =
+        if (BuildConfig.IS_MOCK_ENABLED) {
+            FakeMemoRepoImpl(memoService = memoService)
+        } else {
+            FakeMemoRepoImpl(memoService = memoService)
+        }
 
-    companion object {
-        @Provides
-        fun provideMemoService(): MemoService = RetrofitClient.memoService
+    @Provides
+    @Singleton
+    fun provideMemoService(): MemoService = RetrofitClient.memoService
 
-        @Provides
-        fun provideCartService(): CartService = RetrofitClient.cartService
-    }
+    @Provides
+    @Singleton
+    fun provideCartService(): CartService = RetrofitClient.cartService
 }
