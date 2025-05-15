4d34ff4e003b95c6669d687948cf41d62e4c07ed
Shawn Kang <csm06063@naver.com>
2025-05-16 00:29:23 +0900

    feat: FAB 메뉴 추가

--- src/app/src/main/java/com/imeanttobe/app901/ui/home/HomePage.kt
+++ src/app/src/main/java/com/imeanttobe/app901/ui/home/HomePage.kt
@@ -1,9 +1,18 @@
 package com.imeanttobe.app901.ui.home
 
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.Add
+import androidx.compose.material.icons.rounded.AutoGraph
+import androidx.compose.material3.ExperimentalMaterial3ExpressiveApi
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
@@ -12,15 +21,19 @@ import androidx.hilt.navigation.compose.hiltViewModel
 import com.imeanttobe.app901.R
 import com.imeanttobe.app901.data.enum.HomePageIndex
 import com.imeanttobe.app901.navigation.NavItem
+import com.imeanttobe.app901.ui.component.BottomBar
 import com.imeanttobe.app901.ui.component.Header
 import com.imeanttobe.app901.ui.home.component.history.HistorySection
+import com.imeanttobe.app901.ui.home.component.memo.MemoFloatingActionButtonMenu
 import com.imeanttobe.app901.ui.home.component.memo.MemoSection
 
+@OptIn(ExperimentalMaterial3ExpressiveApi::class)
 @Composable
 fun HomePage(
     navigate: (String) -> Unit,
     viewModel: HomePageViewModel = hiltViewModel(),
 ) {
+    var fabMenuExpanded by remember { mutableStateOf(false) }
     val headerTitle =
         when (viewModel.index.value) {
             HomePageIndex.MEMO_PAGE -> stringResource(R.string.memo)
@@ -31,6 +44,11 @@ fun HomePage(
             HomePageIndex.MEMO_PAGE -> HomePageIndex.HISTORY_PAGE
             HomePageIndex.HISTORY_PAGE -> HomePageIndex.MEMO_PAGE
         }
+    val items =
+        listOf(
+            Icons.Rounded.Add to "메모 추가",
+            Icons.Rounded.AutoGraph to "분석",
+        )
 
     Scaffold(
         topBar = {
@@ -46,6 +64,7 @@ fun HomePage(
         Box(
             modifier =
                 Modifier
+                    .fillMaxSize()
                     .padding(innerPadding)
                     .padding(horizontal = 8.dp),
         ) {
@@ -57,6 +76,12 @@ fun HomePage(
                     HistorySection()
                 }
             }
+
+            MemoFloatingActionButtonMenu(
+                fabMenuExpanded = fabMenuExpanded,
+                setFabMenuExpanded = { newValue -> fabMenuExpanded = newValue },
+                items = items,
+            )
         }
     }
 }
---
+++ src/app/src/main/java/com/imeanttobe/app901/ui/home/component/memo/MemoFloatingActionButtonMenu.kt
@@ -0,0 +1,101 @@
+package com.imeanttobe.app901.ui.home.component.memo
+
+import androidx.activity.compose.BackHandler
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material.icons.filled.Close
+import androidx.compose.material3.ExperimentalMaterial3ExpressiveApi
+import androidx.compose.material3.FloatingActionButtonMenu
+import androidx.compose.material3.FloatingActionButtonMenuItem
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.material3.ToggleFloatingActionButton
+import androidx.compose.material3.ToggleFloatingActionButtonDefaults.animateIcon
+import androidx.compose.material3.animateFloatingActionButton
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.derivedStateOf
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.graphics.vector.rememberVectorPainter
+import androidx.compose.ui.semantics.CustomAccessibilityAction
+import androidx.compose.ui.semantics.contentDescription
+import androidx.compose.ui.semantics.customActions
+import androidx.compose.ui.semantics.isTraversalGroup
+import androidx.compose.ui.semantics.semantics
+import androidx.compose.ui.semantics.stateDescription
+import androidx.compose.ui.semantics.traversalIndex
+
+@OptIn(ExperimentalMaterial3ExpressiveApi::class)
+@Composable
+fun MemoFloatingActionButtonMenu(
+    fabMenuExpanded: Boolean,
+    setFabMenuExpanded: (Boolean) -> Unit,
+    items: List<Pair<ImageVector, String>>,
+) {
+    Box(modifier = Modifier.fillMaxSize()) {
+        BackHandler(enabled = fabMenuExpanded) { setFabMenuExpanded(false) }
+        FloatingActionButtonMenu(
+            modifier = Modifier.align(Alignment.BottomEnd),
+            expanded = fabMenuExpanded,
+            button = {
+                ToggleFloatingActionButton(
+                    modifier =
+                        Modifier
+                            .semantics {
+                                traversalIndex = -1f
+                                stateDescription = if (fabMenuExpanded) "Expanded" else "Collapsed"
+                                contentDescription = "Toggle menu"
+                            }.animateFloatingActionButton(
+                                visible = true,
+                                alignment = Alignment.BottomEnd,
+                            ),
+                    checked = fabMenuExpanded,
+                    onCheckedChange = { setFabMenuExpanded(!fabMenuExpanded) },
+                ) {
+                    val imageVector by remember {
+                        derivedStateOf {
+                            if (checkedProgress > 0.5f) Icons.Filled.Close else Icons.Filled.Add
+                        }
+                    }
+                    Icon(
+                        painter = rememberVectorPainter(imageVector),
+                        contentDescription = null,
+                        modifier = Modifier.animateIcon({ checkedProgress }),
+                    )
+                }
+            },
+        ) {
+            items.forEachIndexed { index, item ->
+                FloatingActionButtonMenuItem(
+                    modifier =
+                        Modifier.semantics {
+                            isTraversalGroup = true
+                            // Add a custom a11y action to allow closing the menu when focusing
+                            // the last menu item, since the close button comes before the first
+                            // menu item in the traversal order.
+                            if (index == items.size - 1) {
+                                customActions =
+                                    listOf(
+                                        CustomAccessibilityAction(
+                                            label = "Close menu",
+                                            action = {
+                                                setFabMenuExpanded(false)
+                                                true
+                                            },
+                                        ),
+                                    )
+                            }
+                        },
+                    onClick = { setFabMenuExpanded(false) },
+                    icon = { Icon(item.first, contentDescription = null) },
+                    text = { Text(text = item.second) },
+                )
+            }
+        }
+    }
+}
