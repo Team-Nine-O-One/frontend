e15e4ae063422ee17abb0bc8e9c3897bdb2d802c
Shawn Kang <csm06063@naver.com>
2025-05-17 11:59:58 +0900

    refactor: Changed the type of createdAt as String

--- src/app/src/main/java/com/imeanttobe/app901/api/response/CreateCartResponse.kt
+++ src/app/src/main/java/com/imeanttobe/app901/api/response/CreateCartResponse.kt
@@ -1,12 +1,11 @@
 package com.imeanttobe.app901.api.response
 
 import com.imeanttobe.app901.data.enum.CartStatus
-import java.time.LocalDateTime
 
 data class CreateCartResponse(
     val cartId: Long,
     val userId: String,
     val memoId: Long,
     val status: CartStatus,
-    val createdAt: LocalDateTime,
+    val createdAt: String,
 )
