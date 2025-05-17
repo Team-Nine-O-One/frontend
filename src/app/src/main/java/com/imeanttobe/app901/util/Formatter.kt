4acb07425a6e98dfe4acb5aadd6433cbe0e524e2
Shawn Kang <77564014+i-meant-to-be@users.noreply.github.com>
2025-05-17 12:08:24 +0900

    [REFACTOR] 시간 포매팅 함수 수정 (#43)

--- src/app/src/main/java/com/imeanttobe/app901/util/Formatter.kt
+++ src/app/src/main/java/com/imeanttobe/app901/util/Formatter.kt
@@ -3,10 +3,18 @@ package com.imeanttobe.app901.util
 import android.content.Context
 import com.imeanttobe.app901.R
 import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
 
 object Formatter {
-    fun dateTimeToString(
+    private val dateTimeFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss")
+
+    fun getModifiedDateString(
         dateTime: LocalDateTime,
         context: Context,
     ): String = context.getString(R.string.format_modified_date, dateTime.monthValue, dateTime.dayOfMonth)
+
+    fun getStringFromLocalDateTime(localDateTime: LocalDateTime): String =
+        localDateTime.format(DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"))
+
+    fun getLocalDateTimeFromString(string: String): LocalDateTime = LocalDateTime.parse(string, dateTimeFormatter)
 }
