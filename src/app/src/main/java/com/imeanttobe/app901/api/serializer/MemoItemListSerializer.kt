d2865cac7ee8ff2394cc81d0f0a3e947cc75e008
Shawn Kang <shineksu813@outlook.com>
2025-05-21 10:06:15 +0900

    refactor: Changed Proto data type's name

--- src/app/src/main/java/com/imeanttobe/app901/api/serializer/MemoItemListSerializer.kt
+++ src/app/src/main/java/com/imeanttobe/app901/api/serializer/MemoItemListSerializer.kt
@@ -6,28 +6,28 @@ import androidx.datastore.core.DataStore
 import androidx.datastore.core.Serializer
 import androidx.datastore.dataStore
 import com.google.protobuf.InvalidProtocolBufferException
-import com.imeanttobe.app901.MemoItemList
+import com.imeanttobe.app901.ProtoMemoItemList
 import java.io.InputStream
 import java.io.OutputStream
 
-object MemoItemListSerializer : Serializer<MemoItemList> {
-    override val defaultValue: MemoItemList = MemoItemList.getDefaultInstance()
+object MemoItemListSerializer : Serializer<ProtoMemoItemList> {
+    override val defaultValue: ProtoMemoItemList = ProtoMemoItemList.getDefaultInstance()
 
-    override suspend fun readFrom(input: InputStream): MemoItemList {
+    override suspend fun readFrom(input: InputStream): ProtoMemoItemList {
         try {
-            return MemoItemList.parseFrom(input)
+            return ProtoMemoItemList.parseFrom(input)
         } catch (exception: InvalidProtocolBufferException) {
             throw CorruptionException("Cannot read proto.", exception)
         }
     }
 
     override suspend fun writeTo(
-        t: MemoItemList,
+        t: ProtoMemoItemList,
         output: OutputStream,
     ) = t.writeTo(output)
 }
 
-val Context.memoItemListDataStore: DataStore<MemoItemList> by dataStore(
+val Context.memoItemListDataStore: DataStore<ProtoMemoItemList> by dataStore(
     fileName = "memo.pb",
     serializer = MemoItemListSerializer,
 )
