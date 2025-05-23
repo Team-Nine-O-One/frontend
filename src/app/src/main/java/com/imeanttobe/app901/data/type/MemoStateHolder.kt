e8bfd8a9ea2069a773bd51cb859e0b561dcbb610
Shawn Kang <shineksu813@outlook.com>
2025-05-23 13:11:09 +0900

    feat: Made MemoStateHolder be thread-safe

--- src/app/src/main/java/com/imeanttobe/app901/data/type/MemoStateHolder.kt
+++ src/app/src/main/java/com/imeanttobe/app901/data/type/MemoStateHolder.kt
@@ -4,6 +4,8 @@ import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateMapOf
 import androidx.compose.runtime.snapshots.SnapshotStateList
 import com.imeanttobe.app901.ProtoMemoItem
+import kotlinx.coroutines.sync.Mutex
+import kotlinx.coroutines.sync.withLock
 import javax.inject.Inject
 
 class MemoStateHolder
@@ -11,34 +13,45 @@ class MemoStateHolder
     constructor(
         private val idGenerator: IdGenerator,
     ) {
+        private val mutex = Mutex()
         val checkStates = mutableStateMapOf<Long, Boolean>()
         val memos: SnapshotStateList<ProtoMemoItem> = mutableStateListOf()
 
-        fun isChecked(id: Long): Boolean = checkStates[id] == true
+        suspend fun isChecked(id: Long): Boolean {
+            mutex.withLock {
+                return checkStates[id] == true
+            }
+        }
 
-        fun setChecked(
+        suspend fun setChecked(
             id: Long,
             value: Boolean,
         ) {
-            checkStates[id] = value
+            mutex.withLock {
+                checkStates[id] = value
+            }
         }
 
-        fun addMemo(item: ProtoMemoItem) {
-            memos.add(item)
-            checkStates[item.id] = false
-            if (item.isLeaf == false) {
-                item.itemsList.forEach {
-                    checkStates[it.id] = false
+        suspend fun addMemo(item: ProtoMemoItem) {
+            mutex.withLock {
+                memos.add(item)
+                checkStates[item.id] = false
+                if (item.isLeaf == false) {
+                    item.itemsList.forEach {
+                        checkStates[it.id] = false
+                    }
                 }
             }
         }
 
-        fun removeMemo(item: ProtoMemoItem) {
-            memos.remove(item)
-            checkStates.remove(item.id)
-            if (item.isLeaf == false) {
-                item.itemsList.forEach {
-                    checkStates.remove(it.id)
+        suspend fun removeMemo(item: ProtoMemoItem) {
+            mutex.withLock {
+                memos.remove(item)
+                checkStates.remove(item.id)
+                if (item.isLeaf == false) {
+                    item.itemsList.forEach {
+                        checkStates.remove(it.id)
+                    }
                 }
             }
         }
