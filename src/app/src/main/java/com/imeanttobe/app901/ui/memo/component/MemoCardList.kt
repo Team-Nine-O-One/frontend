814f0dab46ccf07f6c1bccaec1db3ac578bd18db
Shawn Kang <shineksu813@outlook.com>
2025-05-23 14:30:26 +0900

    feat: Let MemoSection print IconAndText when list is empty

--- src/app/src/main/java/com/imeanttobe/app901/ui/memo/component/MemoCardList.kt
+++ src/app/src/main/java/com/imeanttobe/app901/ui/memo/component/MemoCardList.kt
@@ -2,12 +2,16 @@ package com.imeanttobe.app901.ui.memo.component
 
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.Error
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.imeanttobe.app901.ProtoMemoItem
 import com.imeanttobe.app901.data.type.MemoStateHolder
+import com.imeanttobe.app901.ui.component.IconAndText
 import com.imeanttobe.app901.ui.memo.MemoSection
 import kotlin.collections.forEach
 
@@ -17,29 +21,38 @@ fun MemoCardList(
     memoStateHolder: MemoStateHolder,
     modifier: Modifier = Modifier,
 ) {
-    Column(
-        verticalArrangement = Arrangement.spacedBy(16.dp),
-        modifier = Modifier.then(modifier),
-    ) {
-        memoItems.forEach { item ->
-            if (item.isLeaf) {
-                MemoLeafCard(
-                    item = item,
-                    checked = memoStateHolder.isChecked(item.id),
-                    onCheckedChange = { id, newValue ->
-                        memoStateHolder.setChecked(id, newValue)
-                    },
-                )
-            } else {
-                MemoGroupCard(
-                    item = item,
-                    checked = memoStateHolder.isChecked(item.id),
-                    onCheckedChange = { id, newValue ->
-                        memoStateHolder.setChecked(id, newValue)
-                    },
-                )
+    if (memoItems.isNotEmpty()) {
+        Column(
+            verticalArrangement = Arrangement.spacedBy(16.dp),
+            modifier = Modifier.then(modifier),
+        ) {
+            memoItems.forEach { item ->
+                if (item.isLeaf) {
+                    MemoLeafCard(
+                        item = item,
+                        checked = memoStateHolder.isChecked(item.id),
+                        onCheckedChange = { id, newValue ->
+                            memoStateHolder.setChecked(id, newValue)
+                        },
+                    )
+                } else {
+                    MemoGroupCard(
+                        item = item,
+                        checked = memoStateHolder.isChecked(item.id),
+                        onCheckedChange = { id, newValue ->
+                            memoStateHolder.setChecked(id, newValue)
+                        },
+                    )
+                }
             }
         }
+    } else {
+        IconAndText(
+            icon = Icons.Rounded.Error,
+            text = "장 볼 목록이 비어 있어요.",
+            contentDescription = "Empty memo",
+            modifier = Modifier.fillMaxSize(),
+        )
     }
 }
 
