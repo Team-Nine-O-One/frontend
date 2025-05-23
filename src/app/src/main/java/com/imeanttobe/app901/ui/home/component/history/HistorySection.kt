365ca3f22210e9e58a42df0205be68f04d6bc820
Shawn Kang <csm06063@naver.com>
2025-05-23 10:40:52 +0900

    chore: Deleted trash codes

--- src/app/src/main/java/com/imeanttobe/app901/ui/home/component/history/HistorySection.kt
+++
@@ -1,9 +0,0 @@
-package com.imeanttobe.app901.ui.home.component.history
-
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-
-@Composable
-fun HistorySection() {
-    Text(text = "History Section")
-}
