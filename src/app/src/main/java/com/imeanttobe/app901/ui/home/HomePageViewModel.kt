6c8998633d55a8a0ae27f64248235017cd3efe67
Shawn Kang <shineksu813@outlook.com>
2025-06-02 13:58:56 +0900

    refactor: HomePage의 탭 기본값을 HistorySection으로 변경

--- src/app/src/main/java/com/imeanttobe/app901/ui/home/HomePageViewModel.kt
+++ src/app/src/main/java/com/imeanttobe/app901/ui/home/HomePageViewModel.kt
@@ -18,7 +18,7 @@ class HomePageViewModel
         private val memoRepo: MemoRepo,
     ) : ViewModel() {
         // Variables
-        private val _bottomNavItem = mutableStateOf<BottomNavItem>(BottomNavItem.MemoBottomNavItem)
+        private val _bottomNavItem = mutableStateOf<BottomNavItem>(BottomNavItem.HistoryBottomNavItem)
         private val _fabMenuExpanded = mutableStateOf(false)
         private val _dialogState = mutableStateOf<HomePageDialogState>(HomePageDialogState.NONE)
 
