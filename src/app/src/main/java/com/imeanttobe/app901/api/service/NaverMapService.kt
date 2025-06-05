b5a58b81966156c3ccc254284adc8f7f479ce5ec
Shawn Kang <shineksu813@outlook.com>
2025-06-05 14:32:14 +0900

    refactor: Changed API request url

--- src/app/src/main/java/com/imeanttobe/app901/api/service/NaverMapService.kt
+++ src/app/src/main/java/com/imeanttobe/app901/api/service/NaverMapService.kt
@@ -6,7 +6,7 @@ import retrofit2.http.GET
 import retrofit2.http.Query
 
 interface NaverMapService {
-    @GET("/driving")
+    @GET("/map-direction/v1/driving")
     suspend fun getRoute(
         @Query("start") start: String,
         @Query("goal") goal: String,
