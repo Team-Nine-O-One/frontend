c2436f762bf1db79210f64c68d627f28792021cf
Shawn Kang <shineksu813@outlook.com>
2025-06-05 14:32:58 +0900

    feat: Added a util function on NaverMapRoute

--- src/app/src/main/java/com/imeanttobe/app901/data/model/NaverMapRoute.kt
+++ src/app/src/main/java/com/imeanttobe/app901/data/model/NaverMapRoute.kt
@@ -4,4 +4,7 @@ data class NaverMapRoute(
     val paths: List<Pair<Double, Double>>,
     val distance: Int,
     val duration: Long,
-)
+) {
+    val distanceAsKilometer = distance / 1000.0
+    val durationAsMinute = duration / 1000 / 60
+}
