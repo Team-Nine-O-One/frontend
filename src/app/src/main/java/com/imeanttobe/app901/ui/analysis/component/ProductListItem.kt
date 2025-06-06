8ae67d97cacc6ee82159c8a29a81e1e4f86dbf94
Shawn Kang <csm06063@naver.com>
2025-06-07 00:05:18 +0900

    refactor: 제품 사진 삭제 및 마트 로고 추가

--- src/app/src/main/java/com/imeanttobe/app901/ui/analysis/component/ProductListItem.kt
+++ src/app/src/main/java/com/imeanttobe/app901/ui/analysis/component/ProductListItem.kt
@@ -1,26 +1,18 @@
 package com.imeanttobe.app901.ui.analysis.component
 
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.shape.CircleShape
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.rounded.Error
-import androidx.compose.material.icons.rounded.Image
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.vector.rememberVectorPainter
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import coil3.compose.AsyncImage
 import com.imeanttobe.app901.R
 import com.imeanttobe.app901.data.model.Product
 import com.imeanttobe.app901.ui.component.EmphasizedText
@@ -38,20 +30,6 @@ fun ProductListItem(
         horizontalArrangement = Arrangement.spacedBy(8.dp),
         modifier = modifier,
     ) {
-        // Image
-        AsyncImage(
-            model = imageUrl,
-            contentDescription = "Mart image",
-            placeholder = rememberVectorPainter(image = Icons.Rounded.Image),
-            error = rememberVectorPainter(image = Icons.Rounded.Error),
-            modifier =
-                Modifier
-                    .size(48.dp)
-                    .clip(CircleShape)
-                    .background(color = MaterialTheme.colorScheme.surfaceContainerHighest)
-                    .padding(4.dp),
-        )
-
         // Details of product
         Column(
             modifier = Modifier.weight(1f),
@@ -60,6 +38,9 @@ fun ProductListItem(
                 text = product.name,
                 style = MaterialTheme.typography.titleMedium,
                 color = MaterialTheme.colorScheme.onSurfaceVariant,
+                fontWeight = FontWeight.Bold,
+                maxLines = 1,
+                overflow = TextOverflow.Ellipsis,
             )
             Text(
                 text =
