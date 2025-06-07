5f5680cf2f2edb4b7c7890de514c63c5f85e1ec9
Shawn Kang <csm06063@naver.com>
2025-06-07 14:55:47 +0900

    feat: 오프라인 마트 전용 확장 가능한 카드 구현

--- src/app/src/main/java/com/imeanttobe/app901/ui/analysis/component/OfflineAnalysisCard.kt
+++ src/app/src/main/java/com/imeanttobe/app901/ui/analysis/component/OfflineAnalysisCard.kt
@@ -1,11 +1,19 @@
 package com.imeanttobe.app901.ui.analysis.component
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.unit.dp
 import com.imeanttobe.app901.R
 import com.imeanttobe.app901.data.model.Store
+import com.imeanttobe.app901.ui.component.NaverMap
 
 @Composable
 fun OfflineAnalysisCard(
@@ -19,5 +27,29 @@ fun OfflineAnalysisCard(
                 append(stringResource(R.string.tips_analysis_efficient))
             },
         modifier = modifier,
-    ) { }
+    ) {
+        Column(
+            verticalArrangement = Arrangement.spacedBy(16.dp),
+        ) {
+            NaverMap(
+                modifier =
+                    Modifier
+                        .fillMaxWidth()
+                        .aspectRatio(1f)
+                        .clip(RoundedCornerShape(12.dp)),
+            )
+
+            stores.forEach { store ->
+                OfflineMartCard(
+                    store = store,
+                    products = store.products,
+                ) {
+                    StoreCardDescription(
+                        store = store,
+                        isOffline = true,
+                    )
+                }
+            }
+        }
+    }
 }
