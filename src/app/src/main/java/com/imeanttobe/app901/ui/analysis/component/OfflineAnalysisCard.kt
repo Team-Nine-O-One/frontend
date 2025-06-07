2d5cac2a4440ceadd627eb4d2f2275657c8efdd8
Shawn Kang <csm06063@naver.com>
2025-06-07 17:53:44 +0900

    feat: 설명 추가 및 일부 디자인 조정

--- src/app/src/main/java/com/imeanttobe/app901/ui/analysis/component/OfflineAnalysisCard.kt
+++ src/app/src/main/java/com/imeanttobe/app901/ui/analysis/component/OfflineAnalysisCard.kt
@@ -3,17 +3,23 @@ package com.imeanttobe.app901.ui.analysis.component
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.rounded.ErrorOutline
+import androidx.compose.material.icons.rounded.ExpandMore
 import androidx.compose.material.icons.rounded.LocationOn
 import androidx.compose.material3.ContainedLoadingIndicator
+import androidx.compose.material3.ElevatedCard
 import androidx.compose.material3.ExperimentalMaterial3ExpressiveApi
+import androidx.compose.material3.Icon
 import androidx.compose.material3.LocalContentColor
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
@@ -23,6 +29,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import com.imeanttobe.app901.R
 import com.imeanttobe.app901.data.enum.AnalysisOption
@@ -53,18 +60,12 @@ fun OfflineAnalysisCard(
         Column(
             verticalArrangement = Arrangement.spacedBy(16.dp),
         ) {
-            // Buttons
-            AnalysisOptionButton(
-                selectedOption = selectedOption,
-                onChangeOption = { newOption -> onChangeOption(newOption) },
-                modifier = Modifier.fillMaxWidth(),
-            )
+            // Title
             Text(
-                text = stringResource(R.string.tips_analysis_button_description),
-                style = MaterialTheme.typography.labelMedium,
-                color = LocalContentColor.current.copy(alpha = 0.7f),
+                text = stringResource(R.string.best_route),
+                style = MaterialTheme.typography.titleMedium,
+                fontWeight = FontWeight.Bold,
             )
-            Spacer(modifier = Modifier.height(16.dp))
 
             // Map that prints route
             Box(
@@ -88,13 +89,31 @@ fun OfflineAnalysisCard(
                         )
                     }
                     is ConcurrencyState.Success -> {
-                        NaverMap(
-                            start = stores.first().pos.toLatLng(),
-                            goal = stores.last().pos.toLatLng(),
-                            waypoints = stores.drop(1).dropLast(1).map { it.pos.toLatLng() },
-                            pathPoints = route.paths.map { it.toLatLng() },
-                            modifier = Modifier.fillMaxSize(),
-                        )
+                        Box(modifier = Modifier.fillMaxSize()) {
+                            // Map
+                            // NaverMap(modifier = Modifier.fillMaxSize())
+                            NaverMap(
+                                start = stores.first().pos.toLatLng(),
+                                goal = stores.last().pos.toLatLng(),
+                                waypoints = stores.drop(1).dropLast(1).map { it.pos.toLatLng() },
+                                pathPoints = route.paths.map { it.toLatLng() },
+                                modifier = Modifier.fillMaxSize(),
+                            )
+
+                            // Description
+                            ElevatedCard(
+                                modifier =
+                                    Modifier
+                                        .align(Alignment.BottomCenter)
+                                        .padding(8.dp),
+                            ) {
+                                Text(
+                                    text = "- 거리 1.3km 단축\n- 비용 300원 절감",
+                                    style = MaterialTheme.typography.labelSmall,
+                                    modifier = Modifier.padding(8.dp),
+                                )
+                            }
+                        }
                     }
                     is ConcurrencyState.Default -> {
                         IconAndText(
@@ -106,14 +125,31 @@ fun OfflineAnalysisCard(
                     }
                 }
             }
+            // Buttons
+            AnalysisOptionButton(
+                selectedOption = selectedOption,
+                onChangeOption = { newOption -> onChangeOption(newOption) },
+                modifier = Modifier.fillMaxWidth(),
+            )
             Text(
-                text = stringResource(R.string.tips_analysis_map),
+                text = stringResource(R.string.tips_best_route),
+                style = MaterialTheme.typography.labelMedium,
+                color = LocalContentColor.current.copy(alpha = 0.7f),
+            )
+            Text(
+                text = stringResource(R.string.tips_analysis_button_description),
                 style = MaterialTheme.typography.labelMedium,
                 color = LocalContentColor.current.copy(alpha = 0.7f),
             )
             Spacer(modifier = Modifier.height(16.dp))
 
             // Data about each offline store
+            // Title
+            Text(
+                text = stringResource(R.string.mart_order),
+                style = MaterialTheme.typography.titleMedium,
+                fontWeight = FontWeight.Bold,
+            )
             stores.forEach { store ->
                 OfflineMartCard(
                     store = store,
@@ -124,6 +160,27 @@ fun OfflineAnalysisCard(
                         isOffline = true,
                     )
                 }
+            }
+            Text(
+                text = stringResource(R.string.tips_mart_order),
+                style = MaterialTheme.typography.labelMedium,
+                color = LocalContentColor.current.copy(alpha = 0.7f),
+            )
+            Row(
+                horizontalArrangement = Arrangement.spacedBy(4.dp),
+                verticalAlignment = Alignment.CenterVertically,
+            ) {
+                Icon(
+                    imageVector = Icons.Rounded.ExpandMore,
+                    contentDescription = "Expand",
+                    tint = LocalContentColor.current.copy(alpha = 0.7f),
+                    modifier = Modifier.size(16.dp),
+                )
+                Text(
+                    text = stringResource(R.string.tips_mart_order_products),
+                    style = MaterialTheme.typography.labelMedium,
+                    color = LocalContentColor.current.copy(alpha = 0.7f),
+                )
             }
         }
     }
