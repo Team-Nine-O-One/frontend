b409ba9be1cc712326971c8c3f97b7e7f7ea1419
Shawn Kang <csm06063@naver.com>
2025-06-07 15:22:57 +0900

    chore: 일부 자료형 변경

--- src/app/src/main/java/com/imeanttobe/app901/data/model/Analysis.kt
+++ src/app/src/main/java/com/imeanttobe/app901/data/model/Analysis.kt
@@ -4,7 +4,7 @@ data class Analysis(
     val onlineCount: Int,
     val offlineCount: Int,
     val onlineStore: Store,
-    val offlineStore: Store,
+    val offlineStores: List<Store>,
 ) {
     companion object {
         fun getDefaultInstance(): Analysis =
@@ -12,7 +12,18 @@ data class Analysis(
                 onlineCount = 12,
                 offlineCount = 5,
                 onlineStore = Store.getDefaultInstance(),
-                offlineStore = Store.getDefaultInstance(),
+                offlineStores =
+                    listOf(
+                        Store.getDefaultInstance().copy(
+                            name = "GS더프레시 용산점",
+                            pos = LatAndLng(lat = 37.529036, lng = 126.966016),
+                        ),
+                        Store.getDefaultInstance().copy(
+                            name = "롯데마트 이촌점",
+                            pos = LatAndLng(lat = 37.520996, lng = 126.974770),
+                        ),
+                        Store.getDefaultInstance(),
+                    ),
             )
     }
 }
